3d344425b1ce7f0bcf92b511a8b676907fbae8c1
Taylor Silva <taylorsilva@outlook.com>
2021-05-22 23:49:37 +0100

    Add readme and update script
    Signed-off-by: Taylor Silva <taylorsilva@outlook.com>

--- raspberry-pi/dns-updater/dns-updater.sh
+++ raspberry-pi/dns-updater/dns-updater.sh
@@ -2,14 +2,14 @@
 # See knowlegebase article to config namecheap
 # https://www.namecheap.com/support/knowledgebase/article.aspx/43/11/how-do-i-set-up-a-host-for-dynamic-dns
 # setup cronjob for every 15 minutes `crontab -e`
-# */15 * * * * ddns-update >/dev/null 2>&1
+# */15 * * * * /path/to/dns-updater.sh >/dev/null 2>&1
 
 last_ip_file="/tmp/last_ip"
 last_ip=`cat $last_ip_file`
 echo "DDNS-UPDATE: OK, Getting public IP address"
 ip=$(curl -s http://dynamicdns.park-your-domain.com/getip)
 if [ "$ip" == "$last_ip" ]; then
-	echo "IP Still same, not need to update."
+	echo "IP still the same, no need to update."
 	exit 0
 fi
 
