d149adadfb3a281d1f85a76e52493996846967c0
Taylor Silva <dev@taydev.net>
2022-10-10 12:43:12 -0400

    wip - cloud folder
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- cloud/hetzner.tf
+++ cloud/hetzner.tf
@@ -21,10 +21,11 @@ resource "hcloud_server" "postgres" {
   server_type = "cx31"
   image       = "debian-11"
   location    = "fsn1"
-  # user_data   = "TODO"
+  user_data = file("./postgres.yml")
 
   network {
     network_id = hcloud_network.network.id
+    ip         = "10.0.1.1"
   }
 
   depends_on = [
@@ -41,10 +42,11 @@ resource "hcloud_server" "web-vault" {
   server_type = "cx11"
   image       = "debian-11"
   location    = "fsn1"
-  # user_data   = "TODO"
+  user_data = file("./web-vault.yml")
 
   network {
     network_id = hcloud_network.network.id
+    ip         = "10.0.1.2"
   }
 
   depends_on = [
@@ -61,10 +63,11 @@ resource "hcloud_server" "worker" {
   server_type = "cpx31"
   image       = "debian-11"
   location    = "fsn1"
-  # user_data   = "TODO"
+  user_data = file("./worker.yml")
 
   network {
     network_id = hcloud_network.network.id
+    ip         = "10.0.1.3"
   }
 
   depends_on = [
